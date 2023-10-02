2d65eb0d22560202fe092204eb4604e5103adb2b
alsomeb <alsomeb@gmail.com>
2023-10-02 12:57:35 +0200

    remove comment

--- src/main/java/com/alsomeb/devopsfinal/DevopsFinalApplication.java
+++ src/main/java/com/alsomeb/devopsfinal/DevopsFinalApplication.java
@@ -2,12 +2,7 @@ package com.alsomeb.devopsfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class DevopsFinalApplication {
@@ -15,7 +10,5 @@ public class DevopsFinalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsFinalApplication.class, args);
 	}
-
-	// Test comment to create merge conflict
 
 }
