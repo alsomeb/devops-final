efafedd600abf7393b963c83da5a3bfbc8891430
alsomeb <alsomeb@gmail.com>
2023-10-02 13:00:07 +0200

    remove comment

--- src/main/java/com/alsomeb/devopsfinal/DevopsFinalApplication.java
+++ src/main/java/com/alsomeb/devopsfinal/DevopsFinalApplication.java
@@ -1,21 +1,20 @@
 package com.alsomeb.devopsfinal;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
-public class DevopsFinalApplication {
+public class DevopsFinalApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsFinalApplication.class, args);
 	}
 
-	// Test comment to create merge conflict
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Merge conflict");
+	}
 }
